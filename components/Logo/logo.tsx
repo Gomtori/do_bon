4553d5d7f0f2d9305872fa785c65811425195947
wj <nawonjae@naver.com>
2023-01-11 00:46:51 +0900

    feat(logo): add logo component

--- components/Logo/logo.tsx
+++ components/Logo/logo.tsx
@@ -1,5 +1,38 @@
+import { Box } from '@mui/material'
+import { css } from '@emotion/react'
+import mq from '../../constants/mediaQuery'
+import LogoSvg from '../../public/assets/logo.svg'
+
 const Logo = () => {
-  return <></>
+  return (
+    <Box css={logoBox}>
+      <LogoSvg css={logo} />
+    </Box>
+  )
 }
+
+const logoBox = css`
+  padding: 2.5px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  ${mq[0]} {
+    height: 56px;
+  }
+  ${mq[1]} {
+    height: 70px;
+  }
+`
+
+const logo = css`
+  ${mq[0]} {
+    height: 40px;
+  }
+  ${mq[1]} {
+    height: 45px;
+  }
+`
 
 export default Logo
